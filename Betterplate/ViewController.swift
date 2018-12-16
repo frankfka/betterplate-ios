76d7d2cd3ab73421559a1f4e228ce0ffff4e7443
Frank Jia <jiafrank98@gmail.com>
2018-12-15 21:56:52 -0800

    Initial Commit

---
+++ Betterplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Betterplate
+//
+//  Created by Frank Jia on 2018-12-15.
+//  Copyright © 2018 Frank Jia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
